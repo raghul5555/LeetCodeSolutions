890ee7fb2e08278faf4ba8ef2cc71229b7fe02ff
raghul5555 <raghulsaravanakumar55@gmail.com>
2026-06-24 16:19:03 +0530

    Time: 213 ms (16.83%), Space: 19.3 MB (52.98%) - LeetHub

--- Two Pointers/567. Permutation in String/567. Permutation in String.py
+++ Two Pointers/567. Permutation in String/567. Permutation in String.py
@@ -6,9 +6,10 @@ class Solution:
         for i in s1:
             s1_map[i] = s1_map.get(i, 0) + 1
         win_map = {}
-        for i in range(len(s2)-len(s1)+1):
+        k = len(s1)
+        for i in range(len(s2)-k+1):
             if i==0:
-                for j in range(len(s1)):
+                for j in range(k):
                     win_map[s2[j]] = win_map.get(s2[j], 0) + 1
                 if win_map==s1_map:
                     return True
@@ -16,7 +17,7 @@ class Solution:
                 win_map[s2[i-1]] -= 1
                 if win_map[s2[i-1]]==0:
                     win_map.pop(s2[i-1])
-                win_map[s2[i+len(s1)-1]] = win_map.get(s2[i+len(s1)-1], 0) + 1
+                win_map[s2[i+k-1]] = win_map.get(s2[i+k-1], 0) + 1
                 if win_map == s1_map:
                     return True
             print(s1_map, win_map, i)
